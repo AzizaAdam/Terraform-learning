e6af772a61a0d706165b9812fae13cd94010bfb2
Aziza Adam <95580875+AzizaAdam@users.noreply.github.com>
2023-03-30 14:17:46 +0200

    Update ec2.tf

--- modules/ec2.tf
+++ modules/ec2.tf
@@ -1,50 +1,50 @@
-# # resource "aws_ec2_availability_zone_group" "az_ec2_dev" {
-# #   group_name    = "az_ec2_dev"
-# #   opt_in_status = "opted-in"
-# # }
-
-# resource "aws_ec2_capacity_reservation" "ec2Dev" {
-#   instance_type     = "t2.micro"
-#   instance_platform = "Linux/UNIX"
-#   availability_zone = "eu-west-2a"
-#   instance_count    = 1
+# resource "aws_ec2_availability_zone_group" "az_ec2_dev" {
+#   group_name    = "az_ec2_dev"
+#   opt_in_status = "opted-in"
 # }
 
-# # Create a new host with instance type of c5.18xlarge with Auto Placement
-# # and Host Recovery enabled.
-# resource "aws_ec2_host" "test" {
-#   instance_type     = "c5.18xlarge"
-#   availability_zone = "eu-west-2a"
-#   host_recovery     = "on"
-#   auto_placement    = "on"
-# }
+resource "aws_ec2_capacity_reservation" "ec2Dev" {
+  instance_type     = "t2.micro"
+  instance_platform = "Linux/UNIX"
+  availability_zone = "eu-west-2a"
+  instance_count    = 1
+}
 
-# data "aws_ami" "ubuntu" {
-#   most_recent = true
+# Create a new host with instance type of c5.18xlarge with Auto Placement
+# and Host Recovery enabled.
+resource "aws_ec2_host" "test" {
+  instance_type     = "c5.18xlarge"
+  availability_zone = "eu-west-2a"
+  host_recovery     = "on"
+  auto_placement    = "on"
+}
 
-#   filter {
-#     name   = "name"
-#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-#   }
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
 
-#   owners = ["099720109477"] # Canonical
-# }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-# resource "aws_instance" "instance_dev" {
-#   ami           = data.aws_ami.ubuntu.id
-#   instance_type = "t3.micro"
+  owners = ["099720109477"] # Canonical
+}
 
-#   tags = {
-#     Name = "HelloWorld"
-#   }
-# }
+resource "aws_instance" "instance_dev" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
 
-# resource "aws_ec2_instance_state" "instance_state" {
-#   instance_id = aws_instance.instance_dev.id
-#   state       = "stopped"
-# }
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_ec2_instance_state" "instance_state" {
+  instance_id = aws_instance.instance_dev.id
+  state       = "stopped"
+}
